a39b8f1e672d485214bcb0ccf0e44f17fdb87f64
Evans Murithi <murithievans80@gmail.com>
2018-08-13 20:59:08 +0300

    Ask for confirmation on deletion (#13)

--- src/cmd/delete.rs
+++ src/cmd/delete.rs
@@ -1,5 +1,6 @@
 use clap::{App, Arg, ArgMatches, SubCommand};
 use fs;
+use std::io::{self, Write};
 
 // Create arguments for `delete` subcommand
 pub fn subcommand<'a, 'b>() -> App<'a, 'b> {
@@ -15,18 +16,30 @@ pub fn subcommand<'a, 'b>() -> App<'a, 'b> {
 // Implementation for the `delete` subcommand
 pub fn run(args: &ArgMatches) {
     let account_name = args.value_of("account").unwrap();
-    match fs::read() {
-        Ok(mut accounts) => {
-            if accounts.get(account_name).is_some() {
-                accounts.remove(account_name);
-                match fs::write(&accounts) {
-                    Ok(_) => println!("Account successfully deleted"),
+    print!("Are you sure you want to delete {} [N/y]? ", account_name);
+    io::stdout().flush().unwrap();
+    let mut answer = String::new();
+    match io::stdin().read_line(&mut answer) {
+        Ok(_) => {
+            if answer.trim().to_lowercase() == "y" {
+                match fs::read() {
+                    Ok(mut accounts) => {
+                        if accounts.get(account_name).is_some() {
+                            accounts.remove(account_name);
+                            match fs::write(&accounts) {
+                                Ok(_) => println!("Account successfully deleted"),
+                                Err(err) => eprintln!("{}", err),
+                            };
+                        } else {
+                            println!("Account does not exist");
+                        }
+                    }
                     Err(err) => eprintln!("{}", err),
                 };
             } else {
-                println!("Account does not exist");
+                println!("Abort.");
             }
         }
-        Err(err) => eprintln!("{}", err),
-    }
+        Err(_) => eprintln!("Failed to read input"),
+    };
 }
